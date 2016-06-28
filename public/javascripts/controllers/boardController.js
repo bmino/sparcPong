32376149fd55c80aa4301c4cce2f60af115e0743
bmino <brandonmino@bellsouth.net>
2016-06-28 15:03:20 -0400

    Board Setup
    The board now filters tiers and associated players in each tier. The
    tiers visible is configured through the controller.

--- public/javascripts/controllers/boardController.js
+++ public/javascripts/controllers/boardController.js
@@ -1,5 +1,21 @@
 angular.module('controllers')
-.controller('boardController', ['$scope', function($scope) {
-	$scope.text = 'sample board controller';
+.controller('boardController', ['$scope', 'playerService', function($scope, playerService) {
 	
+	$scope.tiers = generateTiers(5);
+	
+	init();
+	
+	function init() {
+		playerService.getPlayers().then( function(players) {
+			$scope.players = players;
+		});
+	}
+	
+	function generateTiers(tiers) {
+		var arr = [];
+		for (var t=1; t<tiers; t++) {
+			arr.push(t);
+		}
+		return arr;
+	}
 }]);
---
+++ public/javascripts/filters/tier.js
@@ -0,0 +1,44 @@
+angular.module('filters')
+.filter('tier', function() {
+	return function (players, tier) {
+		var tierPlayers = [];
+		
+		if (!players) return tierPlayers;
+		
+		// Calculate possible ranks for the tier
+		var ranks = getRanks(tier);
+		
+		// For each player
+		for (var p=0; p<players.length; p++) {
+			var playerRank = players[p].rank;
+			// For each rank
+			for (var r=0; r<ranks.length; r++) {
+				if (playerRank == ranks[r]) {
+					console.log('Rank '+playerRank+' in ranks '+ranks);
+					tierPlayers.push(players[p]);
+				}
+			}
+		}
+		
+		return tierPlayers.sort(playerSort);
+	}
+	
+	function playerSort(a, b) {
+		return a.rank > b.rank;
+	}
+	
+	function getRanks(tier, currentTier=1, lastRank=0, ranks=[]) {
+		if (ranks.length >= tier) {
+			return ranks;
+		}
+		
+		var ranks = [];
+		for (var r=lastRank+1; r<=lastRank+currentTier; r++) {
+			ranks.push(r);
+		}
+		lastRank = ranks[ranks.length-1];
+		
+		return getRanks(tier, ++currentTier, lastRank, ranks);
+	}
+});
+
