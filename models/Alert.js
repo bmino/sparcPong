61e90db20ee22edce2d2f3152157ed5158b766bc
bmino <brandonmino@bellsouth.net>
2016-09-23 13:02:33 -0400

    Default Alerts Set
    Challenged
    Forfeited

--- models/Alert.js
+++ models/Alert.js
@@ -2,10 +2,10 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var alertSchema = new Schema({
-	challenged: { type: Boolean, default: false },
+	challenged: { type: Boolean, default: true },
 	revoked: { type: Boolean, default: false },
 	resolved: { type: Boolean, default: false },
-	forfeited: { type: Boolean, default: false }
+	forfeited: { type: Boolean, default: true }
 });
 
 var Alert = mongoose.model('Alert', alertSchema);
