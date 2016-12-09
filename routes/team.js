c4c4675ae305fae72c413db079fa5af183187dc2
bmino <brandonmino@bellsouth.net>
2016-12-09 01:42:10 -0500

    Promise Conversion - Team
    Converting team related routes to use promises instead of callbacks

--- routes/team.js
+++ routes/team.js
@@ -25,53 +25,42 @@ router.post('/', function(req, res, next) {
 	
 	var teamUsername = username ? username.trim() : null;
 	
-	validUsername(teamUsername, function(err) {
-		if (err) return next(err);
-		
-		validPlayerTeamsCount(leaderId, function(err, leaderTeams) {
-			if (err) return next(err);
-			validPlayerTeamsCount(partnerId, function(err, partnerTeams) {
-				if (err) return next(err);
-				
-				Player.findById(leaderId, function(err, leader) {
-					if (err) return next(err);
-					Player.findById(partnerId, function(err, partner) {
-						if (err) return next(err);
-						
-						getLowestTeamRank(function(err, lowestRank) {
-							if (err) return next(err);
-							
-							console.log('Creating new team.');
-						
-							// Create new team
-							var team = new Team();
-							team.username = teamUsername;
-							team.leader = leader._id;
-							team.partner = partner._id;
-							team.rank = lowestRank + 1;
-							
-							console.log(team);
-							
-							// Saves team to DB
-							team.save(function(err) {
-								if (err) return next(err);
-								req.app.io.sockets.emit('team:new', teamUsername);
-								console.log('Successfully created a new team.');
-								res.json({message: 'Team created!'});
-							});
-						});
-					});
-				});
-			});
-		});
-	});
-});
+	var validateUsername = validUsername(teamUsername);
+	var validateLeaderTeamsCount = validPlayerTeamsCount(leaderId);
+	var validatePartnerTeamsCount = validPlayerTeamsCount(partnerId);
+	var validateLeader = Player.findById(leaderId).exec();
+	var validatePartner = Player.findById(partnerId).exec();
+	var lowestTeamRank = getLowestTeamRank();
 	
+	Promise.all([validateUsername, validateLeaderTeamsCount, validatePartnerTeamsCount, validateLeader, validatePartner, lowestTeamRank]).then(
+		values => {
+			console.log('Creating new team.');
+			
+			// Create new team
+			var team = new Team();
+			team.username = teamUsername;
+			team.leader = leaderId;
+			team.partner = partnerId;
+			team.rank = values[5] + 1;
+			
+			console.log(team);
+			
+			// Saves team to DB
+			team.save(function(err) {
+				if (err) return next(err);
+				req.app.io.sockets.emit('team:new', teamUsername);
+				console.log('Successfully created a new team.');
+				res.json({message: 'Team created!'});
+			});
+		}, error => {
+			return next(error);
+		});
+});
+
 /* 
  * POST changes team username
  *
  * @param: teamId
- * @param: playerId
  * @param: newUsername
  */
 router.post('/change/username', function(req, res, next) {
@@ -80,22 +69,25 @@ router.post('/change/username', function(req, res, next) {
 	if (!teamId || !playerId)
 		return next(new Error('You must provide a valid player ids.'));
 	
-	validUsername(newUsername, function(err) {
-		if (err) return next(err);
-	
-		console.log('Changing team username.');
-		Team.findById(teamId, function(err, team) {
-			if (err) return next(err);
-			if (playerId != team.leader._id) return next(new Error('Only the team leader can change the team name.'));
-			var oldUsername = team.username;
-			team.username = newUsername;
-			team.save(function(err) {
+	validUsername(newUsername).then(
+		result => {
+			console.log('Changing team username.');
+			Team.findById(teamId, function(err, team) {
 				if (err) return next(err);
-				req.app.io.sockets.emit('team:change:username', {oldUsername: oldUsername, newUsername: newUsername});
-				res.json({message: 'Successfully changed your username from '+ oldUsername +' to '+ newUsername +'!'});
+				if (playerId != team.leader) return next(new Error('Only the team leader can change the team name.'));
+				var oldUsername = team.username;
+				team.username = newUsername;
+				team.save(function(err) {
+					if (err) return next(err);
+					req.app.io.sockets.emit('team:change:username', {oldUsername: oldUsername, newUsername: newUsername});
+					res.json({message: 'Successfully changed your username from '+ oldUsername +' to '+ newUsername +'!'});
+				});
 			});
-		});
-	});
+		},
+		error => {
+			return next(error);
+		}
+	);
 });
 
 
@@ -125,10 +117,14 @@ router.get('/fetch/lookup/:playerId', function(req, res, next) {
 	var playerId = req.params.playerId;
 	if (!playerId) return next(new Error('You must specify a player id.'));
 	
-	getTeamsByPlayerId(playerId, function(err, teams) {
-		if (err) return next(err);
-		res.json({message: teams});
-	});
+	getTeamsByPlayerId(playerId).then(
+		teams => {
+			res.json({message: teams});
+		},
+		error => {
+			return next(error);
+		}
+	);
 });
 
 
@@ -157,92 +153,123 @@ router.get('/record/:teamId', function(req, res, next) {
 	});
 });
 
-function getLowestTeamRank(callback) {
-	Team.find().sort({'rank': -1}).limit(1).exec(function(err, lowestRankTeam) {
-		if (err) {
-			callback(err, null);
-			return;
-		}
-		var lowestRank = 0;
-		if (lowestRankTeam && lowestRankTeam.length > 0) {
-			lowestRank = lowestRankTeam[0].rank;
-		}
-		console.log('Found lowest team rank of ' + lowestRank);
-		callback(err, lowestRank);
+function getLowestTeamRank() {
+	console.log('Looking for lowest team rank.');
+	return new Promise((resolve, reject) => {
+		Team.find().sort({'rank': -1}).limit(1).exec(function(err, lowestRankTeam) {
+			if (err) reject(err);
+			var lowestRank = 0;
+			if (lowestRankTeam && lowestRankTeam.length > 0) {
+				lowestRank = lowestRankTeam[0].rank;
+			}
+			console.log('Found lowest team rank of ' + lowestRank);
+			resolve(lowestRank);
+		});
 	});
 }
 
 var PLAYER_TEAMS_MAX = process.env.PLAYER_TEAM_MAX || 1;
-function validPlayerTeamsCount(playerId, callback) {
-	getTeamsByPlayerId(playerId, function(err, teams) {
-		var count = teams ? teams.length : 0;
-		var plural = PLAYER_TEAMS_MAX > 1 ? 's' : '';
-		console.log('Found ' + count + ' teams associated with this player.');
-		if (count >= PLAYER_TEAMS_MAX) {
-			callback(new Error('Players may not be a part of more than ' + PLAYER_TEAMS_MAX + ' team' + plural + '.'));
-		} else {
-			callback(err, count);
-		}
+function validPlayerTeamsCount(playerId) {
+	console.log('Validating if player is a part of too many teams.');
+	return new Promise((resolve, reject) => {
+		countTeamsByPlayerId(playerId).then(
+			count => {
+				var plural = PLAYER_TEAMS_MAX > 1 ? 's' : '';
+				console.log('Found ' + count + ' teams associated with this player.');
+				if (count >= PLAYER_TEAMS_MAX) {
+					reject(new Error('Players may not be a part of more than ' + PLAYER_TEAMS_MAX + ' team' + plural + '.'));
+				} else {
+					resolve(count);
+				}
+			},
+			error => {
+				reject(error);
+			}
+		);
 	});
 }
 
-function getTeamsByPlayerId(playerId, callback) {
-	if (!playerId) return [];
-	Team.find({$or: [{leader: playerId}, {partner: playerId}]}).exec(function(err, teams) {
-		callback(err, teams);
+function getTeamsByPlayerId(playerId) {
+	return new Promise((resolve, reject) => {
+		Team.find({$or: [{leader: playerId}, {partner: playerId}]}, function(err, teams) {
+			if (err) {
+				reject(err);
+			} else {
+				resolve(teams);
+			}
+		});
+	});
+}
+
+function countTeamsByPlayerId(playerId) {
+	return new Promise((resolve, reject) => {
+		Team.count({$or: [{leader: playerId}, {partner: playerId}]}, function(err, count) {
+			if (err) {
+				reject(err);
+			} else {
+				resolve(count);
+			}
+		});
 	});
 }
 
 var USERNAME_LENGTH_MIN = process.env.USERNAME_LENGTH_MIN || 2;
 var USERNAME_LENGTH_MAX = process.env.USERNAME_LENGTH_MAX || 15;
-function validUsername(username, callback) {
-	console.log('Verifying username of '+ username);
-	
-	if (!username || username == '') {
-		callback(new Error('You must give a username.'));
-		return;
-	}
-	
-	// Allowed character length
-	if (username.length > USERNAME_LENGTH_MAX || username.length < USERNAME_LENGTH_MIN) {
-		callback(new Error('Username length must be between '+ USERNAME_LENGTH_MIN +' and '+ USERNAME_LENGTH_MAX +' characters.'));
-		return;
-	}
-	
-	// No special characters
-	if (!/^[A-Za-z0-9_ ]*$/.test(username)) {
-		callback(new Error('Username can only include letters, numbers, underscores, and spaces.'));
-		return;
-	}
-	
-	// Concurrent spaces
-	if (/\s{2,}/.test(username)) {
-		callback(new Error('You cannot have concurrent spaces.'));
-		return;
-	}
-	
-	// Concurrent underscores
-	if (/_{2,}/.test(username)) {
-		callback(new Error('You cannot have concurrent underscores.'));
-		return;
-	}
-	
-	usernameExists(username, function(err) {
-		callback(err);
+function validUsername(username) {	
+	var checkSyntax = new Promise((resolve, reject) => {
+		console.log('Verifying username of ['+ username +']');
+		
+		if (!username || username == '') {
+			reject(new Error('You must give a username.'));
+		}
+		
+		// Allowed character length
+		if (username.length > USERNAME_LENGTH_MAX || username.length < USERNAME_LENGTH_MIN) {
+			reject(new Error('Username length must be between '+ USERNAME_LENGTH_MIN +' and '+ USERNAME_LENGTH_MAX +' characters.'));
+		}
+		
+		// No special characters
+		if (!/^[A-Za-z0-9_ ]*$/.test(username)) {
+			reject(new Error('Username can only include letters, numbers, underscores, and spaces.'));
+		}
+		
+		// Concurrent spaces
+		if (/\s{2,}/.test(username)) {
+			reject(new Error('Username cannot have concurrent spaces.'));
+		}
+		
+		// Concurrent underscores
+		if (/_{2,}/.test(username)) {
+			reject(new Error('Username cannot have concurrent underscores.'));
+		}
+		
+		resolve(true);
 	});
+	
+	var usernameExists = new Promise((resolve, reject) => {
+		console.log('Checking if team username ['+ username +'] exists.');
+		Team.count({username: username}, function(err, count) {
+			if (err) reject(err);
+			if (!count) {
+				resolve(true);
+			} else {
+				reject(new Error('Username already exists.'));
+			}
+		})
+	});
+	
+	console.log('Validating team username.');
+	return new Promise((resolve, reject) => {
+		Promise.all([checkSyntax, usernameExists]).then(
+			function(success) {
+				resolve(true);
+			},
+			function (error) {
+				reject(error);
+			});
+	});
+	
 }
 
-function usernameExists(username, callback) {
-	console.log('Checking if team username, '+ username +', exists.');
-	Team.count({username: username}, function(err, count) {
-		if (err) {
-			callback(err);
-		} else if (count != 0) {
-			callback(new Error('Username already exists.'));
-		} else {
-			callback(null);
-		}
-	});
-}
 
 module.exports = router;
