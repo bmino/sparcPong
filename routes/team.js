f2e75fb07f3420c17f61ddfab0d5fbcf61f1ecba
Brandon <brandonmino@bellsouth.net>
2017-11-09 00:06:19 -0500

    Teams Win/Loss Display Fix

--- routes/team.js
+++ routes/team.js
@@ -132,7 +132,7 @@ router.get('/record/:teamId', function(req, res, next) {
             var wins = 0;
             var losses = 0;
             challenges.forEach(function(challenge) {
-                if (challenge.winner === teamId) wins++;
+                if (challenge.winner.toString() === teamId) wins++;
                 else losses++;
             });
             res.json({message: {wins: wins, losses: losses}});
