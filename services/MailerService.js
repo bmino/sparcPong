8a0e8450f72da9a9af2c4a041bd240a03f8bb521
Unknown <brandonmino@bellsouth.net>
2017-11-13 16:29:05 -0500

    Modularize MailerService

--- services/MailerService.js
+++ services/MailerService.js
@@ -7,20 +7,35 @@ var Team = mongoose.model('Team');
 var nodemailer = require('nodemailer');
 var xoauth2 = require('xoauth2');
 
-var transporter = nodemailer.createTransport("SMTP", {
-    service: 'gmail',
-    auth: {
-        XOAuth2: {
-            user: process.env.EMAIL_ADDRESS,
-            clientId: process.env.AUTH_CLIENT_ID,
-            clientSecret: process.env.AUTH_CLIENT_SECRET,
-            refreshToken: process.env.AUTH_CLIENT_REFRESH
+
+var MailerService = {
+    newTeamChallenge : newTeamChallenge,
+    revokedTeamChallenge : revokedTeamChallenge,
+    resolvedTeamChallenge : resolvedTeamChallenge,
+    forfeitedTeamChallenge : forfeitedTeamChallenge,
+
+    newChallenge : newChallenge,
+    revokedChallenge : revokedChallenge,
+    resolvedChallenge : resolvedChallenge,
+    forfeitedChallenge : forfeitedChallenge,
+
+    transporter : nodemailer.createTransport("SMTP", {
+        service: 'gmail',
+        auth: {
+            XOAuth2: {
+                user: process.env.EMAIL_ADDRESS,
+                clientId: process.env.AUTH_CLIENT_ID,
+                clientSecret: process.env.AUTH_CLIENT_SECRET,
+                refreshToken: process.env.AUTH_CLIENT_REFRESH
+            }
         }
-    }
-});
+    })
+};
+
+module.exports = MailerService;
 
 
-module.exports.newTeamChallenge = function (teamChallengeId) {
+function newTeamChallenge(teamChallengeId) {
     console.log('Checking email permissions for a new team challenge');
 
     TeamChallenge.populateById(teamChallengeId, true)
@@ -28,16 +43,16 @@ module.exports.newTeamChallenge = function (teamChallengeId) {
             var challenger = challenge.challenger;
             var challengee = challenge.challengee;
             if (challengee.leader.email && challengee.leader.alerts.team.challenged) {
-                sendEmail('Doubles Challenge', 'Your team has been challenged by "'+ challenger.username +'." Log in at http://sparc-pong.herokuapp.com to deal with those scrubs!', challengee.leader.email);
+                sendEmail('Doubles Challenge', 'Your team has been challenged by "'+ challenger.username +'." Log in at ' + process.env.LADDER_URL + ' to deal with those scrubs!', challengee.leader.email);
             }
             if (challengee.partner.email && challengee.partner.alerts.team.challenged) {
-                sendEmail('Doubles Challenge', 'Your team has been challenged by "'+ challenger.username +'." Log in at http://sparc-pong.herokuapp.com to deal with those scrubs!', challengee.partner.email);
+                sendEmail('Doubles Challenge', 'Your team has been challenged by "'+ challenger.username +'." Log in at ' + process.env.LADDER_URL + ' to deal with those scrubs!', challengee.partner.email);
             }
         })
         .catch(console.log);
-};
+}
 
-module.exports.revokedTeamChallenge = function (teamChallengerId, teamChallengeeId) {
+function revokedTeamChallenge(teamChallengerId, teamChallengeeId) {
     console.log('Checking email permissions for a revoked team challenge');
 
     var challengerPromise = Team.findById(teamChallengerId).exec();
@@ -63,9 +78,9 @@ module.exports.revokedTeamChallenge = function (teamChallengerId, teamChallengee
             }
         })
         .catch(console.log);
-};
+}
 
-module.exports.resolvedTeamChallenge = function (teamChallengeId) {
+function resolvedTeamChallenge(teamChallengeId) {
     console.log('Checking email permissions for a resolved team challenge');
 
     TeamChallenge.populateById(teamChallengeId, true)
@@ -78,22 +93,22 @@ module.exports.resolvedTeamChallenge = function (teamChallengeId) {
             var winnerPartner = winner.partner;
 
             if (loserLeader.email && loserLeader.alerts.team.resolved) {
-                sendEmail('Resolved Doubles Challenge', 'Welp, stuff happens. It looks like "'+ winner.username +'" really laid the smack on your team. Log in at http://sparc-pong.herokuapp.com and pick an easier team.', loserLeader.email);
+                sendEmail('Resolved Doubles Challenge', 'Welp, stuff happens. It looks like "'+ winner.username +'" really laid the smack on your team. Log in at ' + process.env.LADDER_URL + ' and pick an easier team.', loserLeader.email);
             }
             if (loserPartner.email && loserPartner.alerts.team.resolved) {
-                sendEmail('Resolved Doubles Challenge', 'Welp, stuff happens. It looks like "'+ winner.username +'" really laid the smack on your team. Log in at http://sparc-pong.herokuapp.com and pick an easier team.', loserPartner.email);
+                sendEmail('Resolved Doubles Challenge', 'Welp, stuff happens. It looks like "'+ winner.username +'" really laid the smack on your team. Log in at ' + process.env.LADDER_URL + ' and pick an easier team.', loserPartner.email);
             }
             if (winnerLeader.email && winnerLeader.alerts.team.resolved) {
-                sendEmail('Resolved Doubles Challenge', 'Congratulations on beating "'+ loser.username +'!" Log in at http://sparc-pong.herokuapp.com to crush some more feelings.', winnerLeader.email);
+                sendEmail('Resolved Doubles Challenge', 'Congratulations on beating "'+ loser.username +'!" Log in at ' + process.env.LADDER_URL + ' to crush some more feelings.', winnerLeader.email);
             }
             if (winnerPartner.email && winnerPartner.alerts.team.resolved) {
-                sendEmail('Resolved Doubles Challenge', 'Congratulations on beating "'+ loser.username +'!" Log in at http://sparc-pong.herokuapp.com to crush some more feelings.', winnerPartner.email);
+                sendEmail('Resolved Doubles Challenge', 'Congratulations on beating "'+ loser.username +'!" Log in at ' + process.env.LADDER_URL + ' to crush some more feelings.', winnerPartner.email);
             }
         })
         .catch(console.log);
-};
+}
 
-module.exports.forfeitedTeamChallenge = function (teamChallengeId) {
+function forfeitedTeamChallenge(teamChallengeId) {
     console.log('Checking email permissions for a forfeited team challenge');
 
     TeamChallenge.populateById(teamChallengeId, true)
@@ -106,22 +121,22 @@ module.exports.forfeitedTeamChallenge = function (teamChallengeId) {
             }
         })
         .catch(console.log);
-};
+}
 
 
-module.exports.newChallenge = function (challengeId) {
+function newChallenge(challengeId) {
     console.log('Checking email permission for a new challenge');
 
     Challenge.populateById(challengeId, true)
         .then(function(challenge) {
             if (challenge.challengee.email && challenge.challengee.alerts.challenged) {
-                sendEmail('New Challenge', 'You have been challenged by '+ challenge.challenger.username +'. Log in at http://sparc-pong.herokuapp.com to deal with that scrub!', challenge.challengee.email);
+                sendEmail('New Challenge', 'You have been challenged by '+ challenge.challenger.username +'. Log in at ' + process.env.LADDER_URL + ' to deal with that scrub!', challenge.challengee.email);
             }
         })
         .catch(console.log);
-};
+}
 
-module.exports.revokedChallenge = function (challengerId, challengeeId) {
+function revokedChallenge(challengerId, challengeeId) {
     console.log('Checking email permission for a revoked challenge');
 
     Promise.all([
@@ -136,9 +151,9 @@ module.exports.revokedChallenge = function (challengerId, challengeeId) {
             }
         })
         .catch(console.log);
-};
+}
 
-module.exports.resolvedChallenge = function (challengeId) {
+function resolvedChallenge(challengeId) {
     console.log('Checking email permission for a resolved challenge');
 
     Challenge.populateById(challengeId, true)
@@ -147,16 +162,16 @@ module.exports.resolvedChallenge = function (challengeId) {
             var winner = challenge.getLoser();
 
             if (loser.email && loser.alerts.resolved) {
-                sendEmail('Resolved Challenge', 'Welp, stuff happens. It looks like '+ winner.username +' really laid the smack on ya. Log in at http://sparc-pong.herokuapp.com and pick an easier opponent.', loser.email);
+                sendEmail('Resolved Challenge', 'Welp, stuff happens. It looks like '+ winner.username +' really laid the smack on ya. Log in at ' + process.env.LADDER_URL + ' and pick an easier opponent.', loser.email);
             }
             if (winner.email && winner.alerts.resolved) {
-                sendEmail('Resolved Challenge', 'Congratulations on beating '+ loser.username +'! Log in at http://sparc-pong.herokuapp.com to crush some more feelings.', winner.email);
+                sendEmail('Resolved Challenge', 'Congratulations on beating '+ loser.username +'! Log in at ' + process.env.LADDER_URL + ' to crush some more feelings.', winner.email);
             }
         })
         .catch(console.log);
-};
+}
 
-module.exports.forfeitedChallenge = function (challengeId) {
+function forfeitedChallenge(challengeId) {
     console.log('Checking email permission for a forfeited challenge');
 
     Challenge.populateById(challengeId, true)
@@ -166,7 +181,7 @@ module.exports.forfeitedChallenge = function (challengeId) {
             }
         })
         .catch(console.log);
-};
+}
 
 
 function sendEmail(subject, message, address) {
@@ -179,7 +194,7 @@ function sendEmail(subject, message, address) {
         text: message
     };
 
-    transporter.sendMail(mailOptions, function(error, response) {
+    MailerService.transporter.sendMail(mailOptions, function(error, response) {
         if (error) return console.log(error);
         console.log('Message sent to ' + address);
     });
