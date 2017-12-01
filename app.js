81d191f703798ef6297c84706fc9ec0d4da07db6
Unknown <brandonmino@bellsouth.net>
2017-11-30 22:48:09 -0500

    Ability to Disable Morgan Logging
    Set MORGAN_FORMAT=none

--- app.js
+++ app.js
@@ -35,7 +35,9 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(favicon(path.join(__dirname, 'public/images', 'favicon.ico')));
-app.use(morgan(process.env.MORGAN_FORMAT || 'tiny'));
+if (process.env.MORGAN_FORMAT !== 'none') {
+	app.use(morgan(process.env.MORGAN_FORMAT || 'tiny'));
+}
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
