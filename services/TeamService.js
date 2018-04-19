6f952b652ebcae7703e77e57e461ac0cd8eae698
Brandon Mino <Mino_Brandon@bah.com>
2018-04-19 11:38:16 -0400

    Properly Reject Promises when Creating Team

--- services/TeamService.js
+++ services/TeamService.js
@@ -15,18 +15,18 @@ function verifyPlayerCanJoinById(playerId) {
     return Promise.all([
         Player.findById(playerId),
         Team.getTeamsByPlayerId(playerId)
-        ])
+    ])
         .then(function(results) {
             var player = results[0];
             var teams = results[1];
-            if (!player) return reject(new Error('Could not find player'));
-            if (!player.active) return reject(new Error('Deactivated players cannot join a team'));
+            if (!player) return Promise.reject(new Error('Could not find player'));
+            if (!player.active) return Promise.reject(new Error('Deactivated players cannot join a team'));
 
             var count = teams.length;
             var plural = TeamService.PLAYER_TEAMS_MAX > 1 ? 's' : '';
             console.log('Found ' + count + ' teams associated with this player.');
-            if (count >= TeamService.PLAYER_TEAMS_MAX) return reject(new Error('Players may not be a part of more than ' + TeamService.PLAYER_TEAMS_MAX + ' team' + plural + '.'));
-            return resolve(count);
+            if (count >= TeamService.PLAYER_TEAMS_MAX) return Promise.reject(new Error('Players may not be a part of more than ' + TeamService.PLAYER_TEAMS_MAX + ' team' + plural + '.'));
+            return Promise.resolve(count);
         })
-        .catch(reject);
+        .catch(Promise.reject);
 }
